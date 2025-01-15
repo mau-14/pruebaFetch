7108d0c7501105afbf0af9f6f612d41d6a47697d
mpenaEVG <mauriciopenadominguez.guadalupe@alumnado.fundacionloyola.net>
2025-01-15 10:26:08 +0100

    select y titulos dinámicos

--- js/controller/c_verAlumnos.js
+++ js/controller/c_verAlumnos.js
@@ -6,14 +6,14 @@ class C_verAlumnos{
   async verAlumnos(){
     const verAlumnos = new M_verAlumnos()
     let alumnos = await verAlumnos.listarAlumnos()
-    console.log(alumnos)
+    return alumnos
 
   }
 
   async verPruebas(){
     const verPruebas = new M_verAlumnos()
     let pruebas = await verPruebas.listarPruebas()
-    console.log(pruebas)
+    return pruebas
 
   }
 }
---
+++ js/mainControlador.js
@@ -0,0 +1,36 @@
+import C_verAlumnos from './controller/c_verAlumnos.js' 
+       let select = document.getElementById('Curso')
+       let div = document.getElementById('Principal')
+       select.addEventListener('change',async (event)=>{
+         const informacion = new C_verAlumnos()
+         let datosAlumnos = await informacion.verAlumnos()
+         let datosPruebas = await informacion.verPruebas()
+         console.log(datosAlumnos)
+         console.log(datosPruebas)
+         
+         const fragment = document.createDocumentFragment()
+         datosPruebas.forEach(dato => {
+              const titulo = document.createElement('h3')
+              titulo.textContent = dato.nombre
+              fragment.appendChild(titulo)
+
+          const select = document.createElement('select')
+          const optionDefault = document.createElement('option')
+          optionDefault.textContent = 'Selecciona un alumno'
+          optionDefault.disabled = true
+          optionDefault.selected = true
+          select.appendChild(optionDefault)
+              datosAlumnos.forEach(dato => {
+                  const option = document.createElement('option')
+                  
+                  option.textContent = dato.nombre
+                  option.value = dato.idAlumno
+                  select.appendChild(option)
+              })
+              fragment.appendChild(select)
+
+         })
+
+         div.appendChild(fragment)
+
+       })
